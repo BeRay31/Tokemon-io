2bd704ff3b4fd28fbe887dad6ef83af89d7b582d
nadiaphi <53868722+nadiaphi@users.noreply.github.com>
2019-11-17 15:59:56 +0700

    keterangan kekuatan dan tipe

--- database.pl
+++ database.pl
@@ -1,19 +1,22 @@
+/*Tipe Tokemon : Fire, Water, Dark, Nightmare, Light*/ 
+/*Urutan kekuatan: Water<Fire<Light<Dark<Nightmare */
+
 tokemon(1,mababoi,dark,430,70,190).
 tokemon(2,bebasmon,light,480,60,120).
 tokemon(3,uts,fire,480,90,190).
 tokemon(4,uas,fire,495,95,220).
 tokemon(5,quiz,water,395,85,185).
-tokemon(6,tubes,leaves,420,75,190).
-tokemon(7,tbfo,leaves,375,95,110).
+tokemon(6,tubes,nightmare,420,75,190).
+tokemon(7,tbfo,nightmare,375,95,110).
 tokemon(8,algeo,fire,385,90,115).
 tokemon(9,alstruk,dark,370,95,120).
 tokemon(10,logkom,fire,390,90,125).
-tokemon(11,aljabar,leaves,370,95,130).
+tokemon(11,aljabar,nightmare,370,95,130).
 tokemon(12,matdis,fire,395,90,135).
 tokemon(13,python,dark,290,5,140).
 tokemon(14,ruby,light,300,10,150).
 tokemon(15,orkom,dark,355,15,160).
-tokemon(16,castlewalls,leaves,415,20,145).
+tokemon(16,castlewalls,nightmare,415,20,145).
 tokemon(17,ekupurushion,dark,425,25,155).
 tokemon(18,matdis,fire,455,90,165).
 tokemon(20,phoenix,fire,310,80,170).
@@ -31,10 +34,10 @@ tokemon(31,shark,water,475,55,245).
 tokemon(32,crocodile,water,485,65,250).
 tokemon(33,bird,water,330,75,255).
 tokemon(34,dragon,fire,495,95,260).
-tokemon(35,eagle,leaves,400,95,265).
+tokemon(35,eagle,nightmare,400,95,265).
 tokemon(36,magika,water,380,5,300).
-tokemon(37,trapie,leaves,390,10,270).
-tokemon(38,snake,leaves,370,15,275).
+tokemon(37,trapie,nightmare,390,10,270).
+tokemon(38,snake,nightmare,370,15,275).
 tokemon(39,ghost,dark,500,20,280).
 tokemon(40,ghoul,dark,475,30,285).
 tokemon(41,angel,light,340,15,290).
@@ -44,7 +47,7 @@ tokemon(44,qilin,light,980,150,225).
 tokemon(45,vermilion,light,990,155,235).
 tokemon(46,azuredragon,fire,970,160,245).
 tokemon(47,panlong,water,965,165,255).
-tokemon(48,tiger,leaves,950,170,265).
+tokemon(48,tiger,nightmare,950,170,265).
 tokemon(49,turtle,water,995,175,210).
 tokemon(50,demon,dark,955,180,200).
 
